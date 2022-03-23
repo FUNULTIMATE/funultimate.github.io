3479de53da313cc44058a82765bec395fc22d61c
FUNULTIMATE <99043550+FUNULTIMATE@users.noreply.github.com>
2022-03-23 11:42:02 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+function savecookie(){
+  document.cookie = "username=John Doe; expires=Thu, 18 Dec 2100 12:00:00 UTC; path=/";
+};
+function changecookie(){
+ document.cookie = "username=John Smith; expires=Thu, 18 Dec 2100 12:00:00 UTC; path=/";
+};
+function deletecookie(){
+  document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+};
