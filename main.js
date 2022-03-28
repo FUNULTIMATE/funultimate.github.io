ee07870d3922ecd11936412d1ba2246f8dd2f870
FUNULTIMATE <99043550+FUNULTIMATE@users.noreply.github.com>
2022-03-28 10:35:17 -0600

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-document.write("your not supposed to be here >:(");
-setTimeout(msg2, 1000);
-function msg2(){
-document.write("go back to cookie clicker or I will have to close this tab!");
-setTimeout(msg3, 1000);  
-};
-function msg3(){
-  close();
-};
